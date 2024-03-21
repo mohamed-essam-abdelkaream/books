af00e76f99206e2a12f0ffb805ede9c482303853
mohamed essam <75342235+mohamed-essam-abdelkaream@users.noreply.github.com>
2024-03-21 23:02:07 +0200

    last update

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor         = Colors.white;
-const Color secondColor        = Color(0xffbc8f35);
+const Color secondColor          = Color(0xffbc8f35);
 const Color readColor            = Color(0xffa30000);
 const Color blueColor            = Color(0xFF5F52EE);
 const Color blackColor           = Color(0xFF3A3A3A);
 const Color greyColor            = Color(0xFF717171);
-const Color darkColor           = Color(0xff0C253F);
+const Color darkColor            = Color(0xff0C253F);
